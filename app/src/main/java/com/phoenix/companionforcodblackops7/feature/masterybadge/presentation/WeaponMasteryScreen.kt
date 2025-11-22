c6f7a105655286b6918cd5c3eb40a65cbdd9a3bb
MasoomBadi <b.masoom@unmiti.com>
2025-11-22 07:52:26 +0530

    modified file

--- app/src/main/java/com/phoenix/companionforcodblackops7/feature/masterybadge/presentation/WeaponMasteryScreen.kt
+++ app/src/main/java/com/phoenix/companionforcodblackops7/feature/masterybadge/presentation/WeaponMasteryScreen.kt
@@ -1,11 +1,9 @@
 package com.phoenix.companionforcodblackops7.feature.masterybadge.presentation
 
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -18,7 +16,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.font.FontWeight
